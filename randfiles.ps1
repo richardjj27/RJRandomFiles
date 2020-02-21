bcf8380663c929b7269e591810bdb68a4cea1a5b
rjohnson <rjohnson@HACHIKO.LOCAL>
2020-02-21 15:35:10 +0000

    Made it a function

--- randfiles.ps1
+++ randfiles.ps1
@@ -1,27 +1,24 @@
 # Provide Folder name and create
-# Test update
-# another edit.
-# xxx ccc
 
-$Folder=’C:\Users\richardjoh\Documents\Projects\hashmove\testfolder’
+function create-testfolders {
+    param([string]$target,[int]$filesize,[int]$numberoffiles)
+    # New-Item -ItemType Directory -Path $Folder
 
-# New-Item -ItemType Directory -Path $Folder
+    # Create a series of 1000 files
+    for ($x=0;$x -lt $numberoffiles; $x++) {
+        # Let’s create a completely random filename
+        $filename1="$($Folder)\"
+        $filename2=-join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})
+        $filename3="$((Get-Random 1000).tostring()).txt"
+        $filename=$filename1+$filename2+$filename3
 
-# Create a series of 1000 files
-for ($x=0;$x -lt 1000; $x++)
+        write-output $filename
+        #write-output $filename2
+        #write-output $filename3
 
-{
-# Let’s create a completely random filename
-$filename1="$($Folder)\"
-$filename2=-join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})
-$filename3="$((Get-Random 1000).tostring()).txt"
-$filename=$filename1+$filename2+$filename3
+        # Now we’ll create the file with some content
+        Add-Content -Value ‘Just a simple demo file’ -Path $filename
+        }
+    }
 
-write-output $filename
-#write-output $filename2
-#write-output $filename3
-
-# Now we’ll create the file with some content
-Add-Content -Value ‘Just a simple demo file’ -Path $filename
-
-}
+create-testfolders('.',1024,10)
